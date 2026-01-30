3a96a8c9bc342f57afc43bdc23da9a6e431c1f84
melkzedek wafula <melkywafula29@gmail.com>
2026-01-30 10:52:50 +0300

    small changes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/items", require("./routes/items"));
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"));
 
