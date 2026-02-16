45796acf411a6445db7fc22fc87a49f7c7c0ab0f
melkzedek wafula <melkywafula29@gmail.com>
2026-02-16 15:49:45 +0300

    changes

---
+++ client/src/pages/Home.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    API.get("/items").then(res => setItems(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Lost & Found Items.</h2>
+      {items.map(item => (
+        <div key={item._id}>
+          <h3>{item.title}</h3>
+          <p>{item.location}</p>
+          <p>{item.number}</p>
+          <p>{item.description}</p>
+          <p>{item.EstimatedPrice}</p>
+          <p>{item.category}</p>
+          <p>{item.status}</p>
+          <p>{item.date}</p>
+          <p>{item.Image}</p>
+          <p>{item.userId}</p>
+          <Link to={`/item/${item._id}`}>View</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
