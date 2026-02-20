2d11d0e6d7b9841c3ad03a1c459bf94ea0bade37
melkzedek wafula <melkywafula29@gmail.com>
2026-02-20 08:37:20 +0300

    changes

--- client/src/pages/ItemDetails.js
+++ client/src/pages/ItemDetails.js
@@ -14,3 +14,16 @@ export default function ItemDetails() {
   if (!item) {
     return <div>Loading...</div>;
   } 
+
+    return (
+    <div>
+      <h2>{item.title}</h2>
+      <p>{item.location}</p>
+        <p>{item.number}</p>    
+        <p>{item.description}</p>
+        <p>{item.EstimatedPrice}</p>
+        <p>{item.category}</p>
+        <p>{item.status}</p>    
+        <p>{item.date}</p>
+        <p>{item.Image}</p>
+        <p>{item.userId}</p>    
