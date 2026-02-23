5963bf6794f9ec0a737db6ea265f113bef722565
melkzedek wafula <melkywafula29@gmail.com>
2026-02-23 14:28:20 +0300

    changes

--- client/src/pages/ItemDetails.js
+++ client/src/pages/ItemDetails.js
@@ -22,6 +22,7 @@ export default function ItemDetails() {
         <p>{item.number}</p>    
         <p>{item.description}</p>
         <p>{item.EstimatedPrice}</p>
+        <p>{item.contact}</p>
         <p>{item.category}</p>
         <p>{item.status}</p>    
         <p>{item.date}</p>
