94ab5ccde1183b3aabc94af567793ddc58c23684
melkzedek wafula <melkywafula29@gmail.com>
2026-02-24 11:15:11 +0300

    changes

---
+++ client/src/pages/ItemDetails.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import { useParams } from "react-router-dom";
+
+export default function ItemDetails() {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+
+    useEffect(() => {   
+    API.get(`/items/${id}`).then(res => setItem(res.data));
+  }, [id]);
+
+  if (!item) {
+    return <div>Loading....</div>;
+  } 
+
+    return (
+    <div>
+      <h2>{item.title}</h2>
+      <p>{item.location}</p>
+        <p>{item.number}</p>    
+        <p>{item.description}</p>
+        <p>{item.EstimatedPrice}</p>
+        <p>{item.contact}</p>
+        <p>{item.category}</p>
+        <p>{item.status}</p>    
+        <p>{item.date}</p>
+        <p>{item.Image}</p>
+        <p>{item.userId}</p>    
+    </div>
+  );
+}
