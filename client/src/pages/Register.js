f481dee189dbc3d00381be95188cd2e60f2ff4f9
Melkzedk <melkywafula29@gmail.com>
2026-04-24 08:05:22 +0300

    changes

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -25,6 +25,11 @@ export default function Register() {
         placeholder="Password"
         onChange={(e) => setForm({ ...form, password: e.target.value })}
       />
+      <input 
+      type="password"
+      placeholder="Confirm Password"
+      onChange={(e) => setForm({ ...form, confirmPassword: e.target.value })}
+      />  
       <button type="submit">Register</button>
     </form>
   );
