2dc1d75d5420110a4a4cd373dac6d32835a1a504
Melkzedk <melkywafula29@gmail.com>
2026-05-12 08:59:12 +0300

    changes

--- client/src/pages/ItemDetails.js
+++ client/src/pages/ItemDetails.js
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 import API from "../services/api";
 import { useParams } from "react-router-dom";
 
+export default function ItemDetails() {
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+
+    useEffect(() => {   
+    API.get(`/items/${id}`).then(res => setItem(res.data));
+  }, [id]);
 
   if (!item) {
     return <div>Loading....</div>;
